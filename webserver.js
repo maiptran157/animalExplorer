36dda44b2823de09d370a91f51ec925e9afd93c8
Mia Tran <maiptran157@gmail.com>
2018-09-07 14:35:23 -0700

    added code to webserver js

--- webserver.js
+++ webserver.js
@@ -1,13 +1,19 @@
-
-//require the animals.js file from the data folder, store it in a variable
-
+//require the animals.js file from the data folder, 
+//store it in a variable
+const animalFile = require('./data/animals.js');
 //require express
-
+const express = require('express');
 //make the server
-
+const server = express();
 //add the static folder and point it at the html folder
-
+server.use(express.static(__dirname + '/html'));
 //make an endpoint to get data for all animals
-	//return the animals data
-
+//return the animals data
+server.get('/', (request, response) => {
+	console.log('Somebody request something');
+	response.send(JSON.stringify(animalFile))
+})
 //make the server listen for connections on port 3000
+server.listen(3000, () => {
+	console.log('server is listening on port 3000')
+})
